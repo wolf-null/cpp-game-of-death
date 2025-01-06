73e82eaee988ea152963dcd27e033766637e6a60
wolf-null <wolf-null@yandex.ru>
2025-01-07 00:50:21 +0300

    initial SFML template

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
+#include <SFML/Graphics.hpp>
+
 #include "topology/tests/test_node.hpp"
 #include "topology/tests/test_grid.hpp"
 #include "topology/tests/test_conway.hpp"
 #include "logger/tests/test_logger.h"
 
-int main() {
+int tests() {
     test_node();
     test_conway();
     test_LogRecord();
@@ -18,4 +20,26 @@ int main() {
         test_grid(3, 5, GridTopology::TORUS).perform_tests();
     }
     return 0;
+}
+
+int main()
+{
+    tests();
+
+    auto window = sf::RenderWindow(sf::VideoMode({800u, 600u}), "GameOfDeath SFML");
+    window.setFramerateLimit(60);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
 }
